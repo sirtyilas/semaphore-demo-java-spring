2d7dc223de434445c91c450f72ee7ed31ac4a1f8
sirtyilas <sirtyilas@yahoo.com>
2019-11-13 10:28:05 +0200

    ADS

---
+++ src/main/java/com/example/springpipelinedemo/tasks/ASD.java
@@ -0,0 +1,9 @@
+package com.example.springpipelinedemo.tasks;
+
+public class ASD {
+
+	public static void main(String[] args) {
+		System.out.println(Runtime.getRuntime().availableProcessors());
+	}
+
+}
